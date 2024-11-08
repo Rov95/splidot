d6d72077f312297ca3fa34c5bcf34dc1dd4b0e86
Rov95 <andres.rov.m.s@gmail.com>
2024-11-08 13:27:06 -0500

    basic sing up component

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
+import ReactDom from 'react-dom';
+import { ClerkProvider } from '@clerk/clerk-react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if(!clerk_key) {
+  throw new Error("No key was found")
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerk_key}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
